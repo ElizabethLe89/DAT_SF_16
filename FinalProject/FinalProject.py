b81283e4fee8d5dc0134321e60c108f6d13b168f
Elizabeth Le <ehle1031@gmail.com>
2015-08-19 18:10:10 -0700

    comment

---
+++ FinalProject/FinalProject.py
@@ -0,0 +1,6 @@
+Title: AirBnb Enhance
+
+Objective: Optimizing your Airbnb stay by comparing price and nearby amenties in San Francisco area
+
+Data sources: https://data.sfgov.org/ , https://www.mashape.com, airbnb.com 
+
