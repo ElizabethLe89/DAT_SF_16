4ad860a34458b153a6edef0d62e440ed71a9412c
Elizabeth Le <ehle1031@gmail.com>
2015-08-25 22:53:42 -0700

    comment

--- homework/soundcloud.py
+++
@@ -1,9 +0,0 @@
-Soundcloud: Question: 
-
-import soundcloud
-
-client = soundcloud.Client(client_id=968035528374afb6797dca3eada16af5)
-
-track = client.get('/resolve', url='http://soundcloud.com/forss/flickermood')
-
-print track.id
